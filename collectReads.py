f8111b646ae030fa0a9561650fdec85c54fddec4
AntonS-bio <47866509+AntonS-bio@users.noreply.github.com>
2022-01-27 09:53:25 +0000

    Updated nucleotide matrix to be Read x MSA length insted of MSA length *5. Analysis relies on hamming distances, not jaccard

---
+++ collectReads.py
@@ -0,0 +1,73 @@
+import pysam as ps
+import random
+
+def getReadsFromBams(validsamples, intervalDepths, config):
+
+    sampleChrRegionReads={}
+    for sample in validsamples:
+        sampleChrRegionReads[sample]={}
+        for chr in intervalDepths:
+            sampleChrRegionReads[sample][chr]={}
+            for region in intervalDepths[chr]:
+                sampleChrRegionReads[sample][chr][region]=[]
+
+    readStartEndToRegion={} #this is for speed as most reads will start and end at the same coordinates. key=(readstart,readend), value=(regionstart,regionend)
+    processedReads=set()#some reads mapped to multple locations, only primary will be used
+    for sample in validsamples:
+        pairedReadIDs={}
+        pairedReadStartEnd={}
+        pairedReadSequences={}
+        bam = ps.AlignmentFile(config.bamDir+sample, "rb",check_sq=False)
+        for read in bam.fetch():
+            readStart=read.reference_start
+            readEnd=read.reference_end
+            if read.is_paired and not read.is_unmapped and not read.mate_is_unmapped and not  read.is_secondary: # is_secondary is false when read is unmapped
+                if read.query_name in pairedReadIDs:
+                    # this is the second of read pairs
+                    # the two reads can overlap, sometimes perfectly
+                    # make final read a composite of both                
+                    fullRead="-"*(max(readStart,readEnd,pairedReadStartEnd[read.query_name][1])-min(readStart,readEnd,pairedReadStartEnd[read.query_name][0]))
+                    if min(readStart,readEnd)<pairedReadStartEnd[read.query_name][0]:
+                        #the current read comes first in the pair
+                        fullread=read.query_alignment_sequence.replace("N","-")+fullRead[len(read.query_alignment_sequence.replace("N","-")):]
+                        fullread=fullRead[ 0: len(fullRead)-pairedReadStartEnd[read.query_name][0] ] + pairedReadSequences[read.query_name]
+                        pairedReadSequences[read.query_name]=fullRead
+                    else:
+                        fullRead=pairedReadSequences[read.query_name]+fullRead[len(pairedReadSequences[read.query_name]):]
+                        fullRead=fullRead[0:len(fullRead)-len(read.query_alignment_sequence.replace("N","-"))] + read.query_alignment_sequence.replace("N","-")
+                        pairedReadSequences[read.query_name]=fullRead
+                    pairedReadIDs[read.query_name]=2
+                    #the second read of the pair has been processed, so set the start/end to be values for the whole pair to be consistent with single end reads
+                    readStart=min(readStart,readEnd,pairedReadStartEnd[read.query_name][0])
+                    readEnd=max(readStart,readEnd,pairedReadStartEnd[read.query_name][1])
+                elif read.is_paired and not read.is_unmapped  and not read.is_secondary:
+                    pairedReadIDs[read.query_name]=1
+                    pairedReadStartEnd[read.query_name]=( min(readStart,readEnd), max(readStart,readEnd) )
+                    pairedReadSequences[read.query_name]=read.query_alignment_sequence.replace("N","-")
+
+            if not read.is_paired or (read.query_name in pairedReadIDs and pairedReadIDs[read.query_name]==2):
+                if bam.get_reference_name(read.reference_id) not in sampleChrRegionReads[sample]:
+                    continue #sometimes spurrious hits are generated on non-target chromosomes
+                if (read.reference_id, readStart, readEnd) not in readStartEndToRegion:
+                    #determine which region the read falls into
+                    for region in sampleChrRegionReads[sample][bam.get_reference_name(read.reference_id)].keys():
+                        if abs(readStart-region[0])<config.maxDistanceBetweenReads and abs(readEnd-region[1])<config.maxDistanceBetweenReads:
+                            readStartEndToRegion[(read.reference_id,readStart, readEnd)]=region
+                            break
+                    if (read.reference_id, readStart, readEnd) not in readStartEndToRegion: #this checks if read in in target regions, if not it is skipped
+                        continue
+                sampleChrRegionReads[sample][bam.get_reference_name(read.reference_id)][readStartEndToRegion[(read.reference_id,readStart, readEnd)]].append(read.query_alignment_sequence.replace("N","-"))
+
+
+    #check if for some sampels reads need to be downsampled
+    for sample in sampleChrRegionReads:
+        for chr in sampleChrRegionReads[sample]:
+            for region in sampleChrRegionReads[sample][chr]:
+                if len(sampleChrRegionReads[sample][chr][region])>config.MaxReadsToOutput:
+                    subset=random.sample(range(0, len(sampleChrRegionReads[sample][chr][region]) ), config.MaxReadsToOutput)
+                    sampleChrRegionReads[sample][chr][region]=[sampleChrRegionReads[sample][chr][region][i] for i in subset]
+
+
+    return sampleChrRegionReads
+    #the reads from samples have been collected into sampleChrRegionReads[sample][chr][region]=[read seqeuences]
+    #print them to fasta and allign using mafft
---
+++ configs/Holly.py
@@ -0,0 +1,10 @@
+class configData:
+    def __init__(self):
+        self.wd = "/mnt/storage5/anton/Amplicon/Holly/"  #directory in which any work will be done file will be
+        self.bamDir = "/mnt/storage5/anton/Amplicon/HollyBam/"  #directory which will be scanned for bam files
+        self.tempDir=self.wd+"/temp/" #directory for temp files
+        self.minRegionDepth=10 #any aggragated region with fewer than this reads will be dropped from further analysis
+        self.maxDistanceBetweenReads=50 #any regions where starts and ends are both fewer than this bases apart are considered one region
+        self.minPosDepthAbsolute=5 #to accomodate high error rate in MinIon, the positions in nucleotide matrix supported by fewer than this reads are dropped
+        self.minPosDepthRelative=0.01 #to accomodate high error rate in MinIon, the positions in nucleotide matrix supported by lower than this % of total reads dropped
+        self.MaxReadsToOutput=2000 #use for downsampling of very large datasets
